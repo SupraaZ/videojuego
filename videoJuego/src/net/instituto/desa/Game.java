9a244e2288e1c68d52dc19d03512b327e5afde42
Adrian Granero <adriangran17@gmail.com>
2020-02-27 09:58:49 +0100

    Refactorización y modificación de los metodos.

--- videoJuego/src/net/instituto/desa/Game.java
+++ videoJuego/src/net/instituto/desa/Game.java
@@ -1,20 +1,12 @@
 package net.instituto.desa;
 
 public class Game {
+	public static final String ABAJO = "Abajo";
+	public static final String ARRIBA = "Arriba";
+	public static final String IZQUIERDA = "Izquierda";
+	public static final String DERECHA = "Derecha";
 	Jugador j;
 
-	public void movimiento(String m) {
-		if (m.equalsIgnoreCase("Derecha")) {
-			j.setX(j.getX() + 1);
-		}
-		if (m.equalsIgnoreCase("Izquierda")) {
-			j.setX(j.getX() - 1);
-		}
-		if (m.equalsIgnoreCase("Arriba")) {
-			j.setY(j.getY() - 1);
-		}
-		if (m.equalsIgnoreCase("Abajo")) {
-			j.setY(j.getY() + 1);
-		}
-	}
+	
+	
 }
